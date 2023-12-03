129f1952e3495d881bd43ef54a87a72a3ae5832a
milosdjurica <milosdjurica13@gmail.com>
2023-12-03 22:22:22 +0100

    abi and binary, script for compile

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19; // solidity version
+pragma solidity ^0.8.7; // solidity version
 
 contract SimpleStorage {
     uint public myFavoriteNumber;
